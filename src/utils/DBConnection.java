f6897e311ac0d82df951b38dd88402f44857d3e9
itsryu <joaovictor9162@outlook.com>
2024-11-28 11:46:47 -0300

    refactor: refactored DBConnection class and added handleError method to handle SQL errors on console.

--- src/utils/DBConnection.java
+++ src/utils/DBConnection.java
@@ -6,15 +6,40 @@ import java.sql.SQLException;
 
 public class DBConnection {
     public static Connection getConnection() {
-        String url = "jdbc:mysql://localhost:3307";
+        String url = "jdbc:mysql://localhost:3306/";
         String user = "root";
-        String password = "catolica";
+        String password = "Jv8272480213#123098ScJv#";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(url, user, password);
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (SQLException error) {
+            DBConnection.handleError(error);
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBC Driver not found: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static void handleError(SQLException error) {
+        int errorCode = Integer.parseInt(error.getSQLState());
+
+        switch (errorCode) {
+            case 28000: {
+                System.err.println("SQL Error: Authentication failed, verify credentials: " + error.getMessage());
+                break;
+            }
+
+            case 42000: {
+                System.err.println("SQL Error: Syntax error: " + error.getMessage());
+                break;
+            }
+
+            default: {
+                System.err.println("SQL Error: " + error.getMessage());
+                break;
+            }
         }
     }
 }
